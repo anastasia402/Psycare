f2d9bb13badbd82d46d3df0402c50c4c32ad3822
anastasiacutulima <anastasia.cutulima4@gmail.com>
2026-01-10 13:43:00 +0200

    added maven+springboot+database

---
+++ src/main/java/com/psycare/PsycareApplication.java
@@ -0,0 +1,11 @@
+package com.psycare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsycareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PsycareApplication.class, args);
+    }
+}
